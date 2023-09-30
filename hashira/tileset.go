f4128c0a2aed6714851ae8307d56fd74786bf4ef
qbart <wojtowicz.bartlomiej@gmail.com>
2023-09-30 10:57:58 +0200

    fix: Add Texture UV epsilon for seamless gap correction

--- hashira/tileset.go
+++ hashira/tileset.go
@@ -17,10 +17,11 @@ func (t *Tileset) TextureUV(tile int, tileWidth int, tileHeight int) (float32, f
 	w := float32(t.Width)
 	h := float32(t.Height)
 
-	u0 := float32(rowX*tileWidth) / w
-	v0 := float32(rowY*tileHeight) / h
-	u1 := float32((rowX+1)*tileWidth) / w
-	v1 := float32((rowY+1)*tileHeight) / h
+	eps := float32(0.0001)
+	u0 := float32(rowX*tileWidth)/w + eps
+	v0 := float32(rowY*tileHeight)/h + eps
+	u1 := float32((rowX+1)*tileWidth)/w - eps
+	v1 := float32((rowY+1)*tileHeight)/h - eps
 
 	return u0, v0, u1, v1
 }
